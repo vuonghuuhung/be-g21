40a9452202b50e221d04a6d0854a0ec8662174b4
huongtx <phamhyta@gmail.com>
2023-06-08 13:04:00 +0700

    order and user api

--- be-g21/app/Http/Controllers/OrderController.php
+++ be-g21/app/Http/Controllers/OrderController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\OrderDetail;
+use App\Models\Product;
+use App\Models\ProductColor;
+use App\Models\ProductStyle;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class OrderController extends BaseController
 {
@@ -28,5 +32,28 @@ class OrderController extends BaseController
         } else {
             return $this->sendResponse('Error', 'Order detail create failed.');
         }
+    }
+
+    public function getListOrder(Request $request)
+    {
+        $page = $request->input('page') ?? 1;
+        $perPage = $request->input('perPage') ?? 10;
+        $orders = Order::get();
+        $orders = new LengthAwarePaginator($orders->forPage($page, $perPage), $orders->count(), $perPage, $page);
+        return $this->sendResponse($orders, 'Orders retrieved successfully.');
+    }
+
+    public function getOrderById($id)
+    {
+        $order = Order::where('id', $id)->get()->first();
+        $products = OrderDetail::where('order_id', $id)->get();
+        if (isset($products)) {
+            foreach ($products as $product) {
+                $p = Product::where('id', $product->product_id)->get()->first();
+                $product->type = $p->option_type == 1 ? ProductColor::where("id", $product->product_detail_id)->get()->first() : ProductStyle::where("id", $product->product_detail_id)->get()->first();
+            }
+        }
+        $order->detail = $products;
+        return $this->sendResponse($order, 'Order retrieved successfully.');
     }
 }
